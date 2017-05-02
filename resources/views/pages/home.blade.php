e07c4fe82d5e16d4726de1bde01ff89586f3c641
funyug <shivamchawla243@gmail.com>
2017-05-03 02:01:08 +0530

    Add thumbnails

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,20 +6,20 @@
         <div class="featured-posts clearfix">
             <div class="col-md-6 slider-div">
                 <a href="/post/{{$featured_posts[0]->slug}}">
-                    <img src="/storage/{{$featured_posts[0]->image}}" class="slider-img-big"/>
+                    <img src="/storage/{{$featured_posts[0]->thumbnail("medium")}}" class="slider-img-big"/>
                     <p class="post-title">{{$featured_posts[0]->title}}</p>
                 </a>
             </div>
             <div class="col-md-6 slider-div">
                 <div class="col-md-12 slider-div">
                     <a href="/post/{{$featured_posts[1]->slug}}">
-                        <img src="/storage/{{$featured_posts[1]->image}}" class="slider-img-small"/>
+                        <img src="/storage/{{$featured_posts[1]->thumbnail("medium")}}" class="slider-img-small"/>
                         <p class="post-title">{{$featured_posts[1]->title}}</p>
                     </a>
                 </div>
                 <div class="col-md-12 slider-div">
                     <a href="/post/{{$featured_posts[2]->slug}}">
-                        <img src="/storage/{{$featured_posts[2]->image}}" class="slider-img-small"/>
+                        <img src="/storage/{{$featured_posts[2]->thumbnail("medium")}}" class="slider-img-small"/>
                         <p class="post-title">{{$featured_posts[2]->title}}</p>
                     </a>
                 </div>
@@ -34,7 +34,7 @@
                 <a href="/post/{{$post->slug}}">
                 <div class="col-md-6 post">
                     <div class="col-md-4 post-image">
-                        <img src="/storage/{{$post->image}}">
+                        <img src="/storage/{{$post->thumbnail("small")}}">
                     </div>
                     <div class="col-md-8">
                         <h1 class="post-title">{{$post->title}}</h1>
