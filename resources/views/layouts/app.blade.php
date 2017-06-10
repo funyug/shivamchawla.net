b1fe255edab8d095d971c2b7d09da06e982a9434
funyug <shivamchawla243@gmail.com>
2017-06-10 21:18:45 +0530

    Add head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,5 @@
 <html ng-app="app">
+<head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,6 +26,7 @@
     ga('send', 'pageview');
 
 </script>
+</head>
 <body>
 <div class='navbar navbar-fixed-top'>
     <nav class='navbar-inner header'>
